d4a62e526b647cfa79d45ffa3a047af2de8d33cc
gerritjvv <gerritjvv@gmail.com>
2014-01-14 11:11:45 +0100

    update version

--- kafka-clj/project.clj
+++ kafka-clj/project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-clj "0.1.9-SNAPSHOT"
+(defproject kafka-clj "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
