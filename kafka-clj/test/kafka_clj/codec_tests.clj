05c0d424a1687716f72ffbe1387f968af0e0e67a
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2014-08-12 22:03:16 +0200

    add tests

---
+++ kafka-clj/test/kafka_clj/codec_tests.clj
@@ -0,0 +1,14 @@
+(ns kafka-clj.codec-tests
+  (:require [kafka-clj.codec :refer :all])
+  (:use midje.sweet))
+
+
+(facts "Test codecs"
+       
+       (fact "Test crc32"
+             (crc32-int (.getBytes "hi")) => (complement nil?) )
+       (fact "Get codec ints"
+             (get-codec-int "snappy") => 2
+             (get-codec-int "gzip") => 1
+             (get-codec-int "none") => 0
+             (get-codec-int "other") => (throws Exception) ))
