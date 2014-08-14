d16f0f3718820607d9f6363fbedacf0d768b36e4
gerritjvv <gerritjvv@gmail.com>
2014-08-14 23:00:17 +0100

    up version

--- kafka-clj/project.clj
+++ kafka-clj/project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-clj "2.3.1"
+(defproject kafka-clj "2.3.2"
   :description "fast kafka library implemented in clojure"
   :url "https://github.com/gerritjvv/kafka-fast"
   :license {:name "Eclipse Public License"
