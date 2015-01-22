79e11bf0825e75c8e20e4c1669bc5f5988dc956e
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2015-01-22 23:05:30 +0100

    update meta to not nil?

--- kafka-clj/src/kafka_clj/metadata.clj
+++ kafka-clj/src/kafka_clj/metadata.clj
@@ -96,7 +96,7 @@
     (filter (complement nil?))
     (filter (complement #(is-blacklisted? % @blacklisted-metadata-producers-ref)))
     (map #(_get-metadata % conf blacklisted-metadata-producers-ref))
-    (filter (fn [[_ meta]] (nil? meta)))
+    (filter (fn [[_ meta]] (not (nil? meta))))
     first))
 
 (defn get-metadata [metadata-producers conf & {:keys [blacklisted-metadata-producers-ref] :or {blacklisted-metadata-producers-ref (ref {})}}]
