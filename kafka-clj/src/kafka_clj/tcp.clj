e6ef19958d85712aa9d18c2e8b1357b5f626f294
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2015-02-18 23:36:23 +0100

    use blocking io for sending to avoid memory leak and with same performance as nio

---
+++ kafka-clj/src/kafka_clj/tcp.clj
@@ -0,0 +1,83 @@
+(ns kafka-clj.tcp
+  (:require [clojure.tools.logging :refer [error info]])
+  (:import (java.net Socket SocketException)
+           (java.io InputStream OutputStream BufferedInputStream BufferedOutputStream DataInputStream)
+           (io.netty.buffer ByteBuf)))
+
+
+;; Simple Direct TCP Client for the producer
+;; The producers sit behind an async buffer where data is pushed on
+;; by multiple threads, the TCP sending itself does not need yet another layer or indirection
+;; which at the current moment under high loads with huge messages can cause out of memory errors
+
+;;; Usage
+;;; (def client (tcp/tcp-client "localhost" 7002))
+;;; (tcp/write! client "one two three" :flush true)
+;;; (tcp/read-async-loop! client (fn [^"[B" bts] (prn (String. bts))))
+;;; (tcp/close! client)
+
+(defrecord TCPClient [host port conf socket ^BufferedInputStream input ^BufferedOutputStream output])
+
+(defprotocol TCPWritable (-write! [obj tcp-client] "Write obj to the tcp client"))
+
+(defn tcp-client
+  "Creates a tcp client from host port and conf
+   InputStream is DataInputStream(BufferedInputStream) and output is BufferedOutputStream"
+  [host port & conf]
+  {:pre [(string? host) (number? port)]}
+  (let [socket (Socket. (str host) (int port))]
+    (.setSendBufferSize socket (int (* 1048576 2)))
+    (.setReceiveBufferSize socket (int (* 1048576 2)))
+
+    (->TCPClient host port conf socket
+                 (DataInputStream. (BufferedInputStream. (.getInputStream socket)))
+                 (BufferedOutputStream. (.getOutputStream socket)))))
+
+(defn read-async-loop!
+  "Only call this once on the tcp-client, it will create a background thread that exits when the socket is closed.
+   The message must always be [4 bytes size N][N bytes]"
+  [{:keys [^Socket socket ^DataInputStream input]} handler]
+  {:pre [socket input (fn? handler)]}
+  (future
+    (try
+      (while (not (.isClosed socket))
+        (let [size (.readInt input)
+              bts (byte-array size)
+              _   (.read input bts)]
+          (try
+            (handler bts)
+            (catch SocketException e nil)
+            (catch Exception e (error e e)))))
+      (catch SocketException e nil))))
+
+(defn write! [tcp-client obj & {:keys [flush] :or {flush false}}]
+  (when obj
+    (-write! obj tcp-client)
+    (if flush
+      (.flush ^BufferedOutputStream (:output tcp-client)))))
+
+(defn close! [{:keys [^Socket socket ^InputStream input ^OutputStream output]}]
+  {:pre [socket]}
+  (info "tcp/close!: socket: " (.isClosed socket))
+  (when (not (.isClosed socket))
+    (.flush output)
+    (.close output)
+    (.close input)
+    (.close socket)))
+
+(defn- _write-bytes [tcp-client ^"[B" bts]
+  (.write ^BufferedOutputStream (:output tcp-client) bts))
+
+(extend-protocol TCPWritable
+  (Class/forName "[B")
+  (-write! [obj tcp-client]
+    (_write-bytes tcp-client obj))
+  ByteBuf
+  (-write! [obj tcp-client]
+    (let [^ByteBuf buff obj
+          readable-bytes (.readableBytes buff)]
+      (.readBytes buff ^OutputStream (:output tcp-client) (int readable-bytes))))
+  String
+  (-write! [obj tcp-client]
+    (_write-bytes tcp-client (.getBytes ^String obj "UTF-8"))))
+
---
+++ kafka-clj/test/kafka_clj/tcp_tests.clj
@@ -0,0 +1,34 @@
+(ns kafka-clj.tcp-tests
+  (:require [kafka-clj.tcp :as tcp]
+            [clj-tcp.echo-server :as server])
+  (:use midje.sweet))
+
+
+;;; Test the kafka-clj.tcp connections
+
+(def test-facts (ref {}))
+
+(defn start-server []
+  (server/start-server 7009))
+
+(defn start-test-facts! []
+  (dosync
+    (alter test-facts (fn [m]
+                        (assoc m :server (start-server))))))
+
+(defn stop-test-facts! []
+  (server/close-server (get @test-facts :server)))
+
+(with-state-changes
+  [(before :facts (start-test-facts!))
+   (after  :facts (stop-test-facts!))]
+
+  (fact "Send byte array data"
+        (let [data (promise)
+              client (tcp/tcp-client "localhost" 7009)
+              f (tcp/read-async-loop! client (fn [v] (deliver data (String. ^"[B" v))))]
+
+          (tcp/write! client (.getBytes "hi") :flush true)
+          @data => "hi"
+
+          (tcp/close! client))))
