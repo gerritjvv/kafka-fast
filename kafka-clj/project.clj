67a528e60859a0d221f41d8ad858cb4a0c536905
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2015-07-01 11:48:36 +0200

    add back commented public functions

--- kafka-clj/project.clj
+++ kafka-clj/project.clj
@@ -1,4 +1,4 @@
-(defproject kafka-clj "3.1.1-SNAPSHOT"
+(defproject kafka-clj "3.1.2-SNAPSHOT"
   :description "fast kafka library implemented in clojure"
   :url "https://github.com/gerritjvv/kafka-fast"
   :license {:name "Eclipse Public License"
