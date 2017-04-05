8b64f940d9bc5d792705f88c388b5e9dc15aac72
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2017-04-05 17:06:36 +0200

    revert use earliest and update max-offsets

--- kafka-clj/src/kafka_clj/fetch.clj
+++ kafka-clj/src/kafka_clj/fetch.clj
@@ -102,7 +102,7 @@
   (with-size buff write-fetch-request-header topics state))
 
 
-(defn write-offset-request-message [^ByteBuf buff {:keys [topics use-earliest max-offsets] :or {max-offsets 10 use-earliest false}}]
+(defn write-offset-request-message [^ByteBuf buff {:keys [topics max-offsets] :or {max-offsets 500}}]
   "
 	OffsetRequest => ReplicaId [TopicName [Partition Time MaxNumberOfOffsets]]
 	  ReplicaId => int32
@@ -122,7 +122,7 @@
       (do
         (-> buff
             (.writeInt (int partition))
-            (.writeLong (if use-earliest -2 -1))
+            (.writeLong -1)
             (.writeInt (int max-offsets))))))
 
   buff)
